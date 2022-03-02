25a2e27714c6828175c0fad450b9ebac68c38034
HANSOL <soool1021@gmail.com>
2022-03-02 20:24:24 +0900

    03-02

--- server/app.js
+++ server/app.js
@@ -1,7 +1,26 @@
 import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import config from "./config";
 
 const app = express();
+const { MONGO_URI } = config;
 
+app.use(hpp());
+app.use(helmet());
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connecting Success!!"))
+  .catch((e) => consol.log(e));
 app.get("/");
 
 export default app;
